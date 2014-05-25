e16e97bbfaba13bf8592d006e35a65c93a8614ae
dyslexicanaboko@gmail.com <dyslexicanaboko@gmail.com@80621dee-4b5a-ebd3-7c08-5ff25b41b84b>
2014-05-25 03:40:00 +0000

    Uploading WCF Services Security example for blog

---
+++ trunk/WCFServicesSecurity/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+
+namespace WCFServicesSecurity
+{
+    public static class Config
+    {
+        public static string HeaderName { get { return GetRawConfigValue("HeaderName"); } }
+        public static string HeaderNamespace { get { return GetRawConfigValue("HeaderNamespace"); } }
+        public static string AuthorizationTokenName { get { return GetRawConfigValue("AuthorizationTokenName"); } }
+        public static string AuthorizationTokenValue { get { return GetRawConfigValue("AuthorizationTokenValue"); } }
+
+        /// <summary>
+        /// Retrieves value from ConfigurationManager.AppSettings.
+        /// Throws exception if the value returned is null or whitespace.
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <returns>Value</returns>
+        private static string GetRawConfigValue(string keyName)
+        {
+            string strValue = ConfigurationManager.AppSettings[keyName];
+
+            //Check if the value returned is null or whitespace, in other words invalid
+            if (String.IsNullOrWhiteSpace(strValue))
+                throw new Exception(string.Format("The key [{0}] was not found or its value was blank. Please add this entry --> <add key=\"{0}\" value=\"\" />", keyName));
+
+            return strValue;
+        }
+    }
+}
