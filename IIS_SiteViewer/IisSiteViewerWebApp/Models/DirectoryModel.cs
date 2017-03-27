c14029a612bc670193042697fdb1f5e6d0505f70
Eli Hayon <dyslexicanaboko@gmail.com>
2017-03-26 23:56:43 -0400

    Adding IIS Siteview for the first time

---
+++ IIS_SiteViewer/IisSiteViewerWebApp/Models/DirectoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IisManagementTools;
+using Microsoft.Web.Administration;
+using System.Web.Mvc;
+
+namespace IisSiteViewerWebApp.Models
+{
+    public class DirectoryModel
+    {
+        public ProcessModelIdentityType? IdentityType { get; set; }
+
+        public string ServiceAccount { get; set; }
+    }
+}
