bfb4120edada2865ab0e9993cc28df9d2867dab4
Eli Hayon <dyslexicanaboko@gmail.com>
2017-03-27 00:17:36 -0400

    I am very sure I screwed up this code base. I am going to have to review this code again at a later time to really clean and fix it up.

---
+++ Id3v1Tags/Id3v1TagAdjuster.NUnit.Tests/TagAdjusterTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace Id3v1TagAdjuster.NUnit.Tests
+{
+    [TestFixture]
+    public class TagAdjusterTests
+    {
+        [Test, Ignore]
+        [TestCase(@"F:\Dump\ID3v1 tag test\Maximum The Hormone - マキシマム ザ ホルモン", 1, 7)]
+        public void TargetDirectoryContains_X_Mp3s(string directory, int albums, int tracks)
+        { 
+            //Given that I have a target album with MP3s in it
+
+            //When I query the target directory
+            var obj = new TagAdjuster();
+
+            var a = new Artist(directory, false);
+
+            obj.FixTags(a);
+
+            //Then it has an expected number of tracks in it
+            Assert.IsTrue(a.Albums.Count == albums);
+            Assert.IsTrue(a.Albums[0].Tracks.Count == tracks);
+        }
+
+        [Test]
+        [TestCase(@"G:\CD Data\Music\Maximum The Hormone - マキシマム ザ ホルモン", 6, 61)]
+        public void FixTracks(string path, int numberOfAlbums, int totalTracks)
+        {
+            //Given that I have a target Artist
+
+            //When I look at the artist's directory
+            var a = new Artist(path, false);
+
+            new TagAdjuster().FixTags(a);
+
+            //Then I should get a count of albums
+            Assert.IsTrue(a.Albums.Count == numberOfAlbums);
+            Assert.IsTrue(a.CountTotalTracks() == totalTracks);
+        }
+
+        [Test, Ignore]
+        [TestCase(@"G:\CD Data\Music\Maximum The Hormone - マキシマム ザ ホルモン", 6, 61)]
+        public void ArtistObjectIsLoadedMultipleAlbumsAndTracks(string path, int numberOfAlbums, int totalTracks)
+        {
+            //Given that I have a target Artist
+
+            //When I look at the artist's directory
+            var a = new Artist(path, false);
+
+            //Then I should get a count of albums
+            Assert.IsTrue(a.Albums.Count == numberOfAlbums);
+            Assert.IsTrue(a.CountTotalTracks() == totalTracks);
+        }
+
+        [Test, Ignore]
+        [TestCase(@"G:\CD Data\Music\Maximum The Hormone - マキシマム ザ ホルモン", 6)]
+        public void ArtistObjectIsLoadedWithNameAndAlbumNames(string path, int numberOfAlbums)
+        { 
+            //Given that I have a target Artist
+
+            //When I look at the artist's directory
+            var obj = new TagAdjuster();
+            var a = new Artist(path, false);
+
+            //Then I should get a count of albums
+            Assert.IsTrue(a.Albums.Count == numberOfAlbums);
+        }
+
+        [Test, Ignore]
+        [TestCase(@"G:\CD Data\Music\Maximum The Hormone - マキシマム ザ ホルモン\2001 鳳 - Ootori", 2001, "鳳 - Ootori", 7)]
+        public void AlbumIsLoadedWithTracks(string path, int year, string name, int numberOfTracks)
+        {
+            //Given that I have a target Artist
+
+            //When I look at the artist's directory
+            var obj = new TagAdjuster();
+            var a = new Album(null, path, false);
+
+            //Then I should get a count of albums
+            Assert.IsTrue(a.Year == year);
+            Assert.IsTrue(a.Name == name);
+            Assert.IsTrue(a.Tracks.Count == numberOfTracks);
+        }
+    }
+}
---
+++ Id3v1Tags/Id3v1TagAdjuster/TrackMover.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Id3v1TagAdjuster
+{
+    public class TrackMover
+    {
+        private Track _track;
+
+        public string NewTrackName { get; private set; }
+
+        public bool IsMoved { get; private set; }
+
+        public bool IsCopied { get; private set; }
+
+        public Exception Error { get; private set; }
+
+        public TrackMover(Track track)
+        {
+            _track = track;
+        }
+
+        public bool Move(string targetPath)
+        {
+            IsMoved = FileOperation(targetPath, File.Move);
+
+            return IsMoved;
+        }
+
+        public bool Copy(string targetPath)
+        {
+            IsCopied = FileOperation(targetPath, File.Copy);
+
+            return IsCopied;
+        }
+
+        private bool FileOperation(string targetPath, Action<string, string> method)
+        {
+            bool fileOperationResult = false;
+
+            try
+            {
+                //Initialize
+                int i = 0;
+
+                NewTrackName = _track.GetArtistAlbumTrackString();
+
+                string strTarget = Path.Combine(targetPath, NewTrackName);
+
+                while (File.Exists(strTarget))
+                {
+                    i++;
+
+                    NewTrackName = _track.GetArtistAlbumTrackString("d" + i.ToString());
+
+                    //Try again
+                    strTarget = Path.Combine(targetPath, NewTrackName);
+                }
+
+                if (!File.Exists(_track.FullPath))
+                    return false;
+
+                method(_track.FullPath, strTarget);
+
+                fileOperationResult = true;
+            }
+            catch (Exception ex)
+            {
+                Error = ex;
+
+                fileOperationResult = false;
+            }
+
+            return fileOperationResult;
+        }
+    }
+}
