63f701979f8635cf4cacc064912b6308f436bb66
Eli Hayon <dyslexicanaboko@gmail.com>
2017-04-07 23:34:42 -0400

    Dependency injection practice with Simple Injector.

---
+++ DiPracticeTest/SaveData/SaveToFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiPracticeTest.SaveData
+{
+    public class SaveToFile : ISaveData
+    {
+        public void Save(List<string> data)
+        {
+            System.Diagnostics.Debug.WriteLine(this.GetType().Name);
+        }
+    }
+}
