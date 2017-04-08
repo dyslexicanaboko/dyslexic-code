6a003686ef805aea08f6541f0b1789f729af59a0
Eli Hayon <dyslexicanaboko@gmail.com>
2017-04-08 00:34:42 -0400

    Adding an EF code first example.

---
+++ EfCodeFirstPractice/Context/EmailContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EfCodeFirstPractice.Models;
+
+namespace EfCodeFirstPractice.Context
+{
+    public class EmailContext : DbContext
+    {
+        public DbSet<Email> Email { get; set; }
+
+        public DbSet<Attachment> Attachments { get; set; }
+    }
+}
