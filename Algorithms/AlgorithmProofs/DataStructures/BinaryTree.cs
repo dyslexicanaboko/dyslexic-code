bc91d4394d577ee8460e01300bf717b7c1c6907c
Eli Hayon <dyslexicanaboko@gmail.com>
2018-08-07 21:27:01 -0400

    Adding a BinaryTree for practice.

---
+++ Algorithms/AlgorithmProofs/DataStructures/BinaryTree.cs
@@ -0,0 +1,97 @@
+﻿namespace AlgorithmProofs.DataStructures
+{
+    public class BinaryTree
+    {
+        public void Add(int number)
+        {
+            Count++;
+
+            if (Root == null)
+            {
+                Root = new Node(number);
+
+                return;
+            }
+
+            Add(Root, number);
+        }
+
+        private void Add(Node parent, int number)
+        {
+            if (number < parent.Number)
+            {
+                if (parent.Left == null)
+                {
+                    parent.Left = new Node(parent, number);
+                }
+                else
+                {
+                    Add(parent.Left, number);
+                }
+            }
+            else
+            {
+                if (parent.Right == null)
+                {
+                    parent.Right = new Node(parent, number);
+                }
+                else
+                {
+                    Add(parent.Right, number);
+                }
+            }
+        }
+        
+        public void Remove(int number)
+        {
+            Count--;
+        }
+
+        public Node Root { get; private set; }
+
+        public int Count { get; set; }
+
+        //public override string ToString()
+        //{
+            
+        //}
+
+        //private StringBuilder PrettyPrint(Node node)
+        //{
+
+        //}
+
+        //private StringBuilder PrettyPrint(Node node, StringBuilder stringBuilder)
+        //{
+
+        //}
+
+        public class Node
+        {
+            public Node(int number)
+                : this(null, number)
+            {
+
+            }
+
+            public Node(Node parent, int number)
+            {
+                Parent = parent;
+
+                Depth = (parent?.Depth + 1) ?? 0;
+
+                Number = number;
+            }
+
+            public int Number { get; set; }
+
+            public int Depth { get; set; }
+
+            public Node Parent { get; set; }
+
+            public Node Left { get; set; }
+
+            public Node Right { get; set; }  
+        }
+    }
+}
--- Algorithms/AlgorithmProofsConsole/Program.cs
+++ Algorithms/AlgorithmProofsConsole/Program.cs
@@ -2,12 +2,29 @@
 using AlgorithmProofs.Sorting;
 using System;
 using System.Collections.Generic;
+using AlgorithmProofs.DataStructures;
 
 namespace AlgorithmProofsConsole
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var bt = new BinaryTree();
+
+            bt.Add(2);
+            bt.Add(1);
+            bt.Add(3);
+
+            Console.WriteLine($"{bt.Root.Number}");
+            Console.WriteLine($"{bt.Root.Left.Number}");
+            Console.WriteLine($"{bt.Root.Right.Number}");
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static void SortingTests()
         {
             var lst = new List<SortingAlgorithmBase>
             {
@@ -27,15 +44,12 @@ namespace AlgorithmProofsConsole
 
             //Generate a new large sequence and don't print anything
             //var s = new Sequence(showUnsortedArray: false, generateNewSequence: true, arraySize: 1000) { ShowSortedResult = false };
-            
+
             //Run the current sequence and don't print anything
             //var s = new Sequence(showUnsortedArray: false) { ShowSortedResult = false };
 
 
             lst.ForEach(x => s.TestSortAlgorithm(x));
-            
-            Console.WriteLine("Press any key to continue");
-            Console.ReadKey();
         }
     }
 }
