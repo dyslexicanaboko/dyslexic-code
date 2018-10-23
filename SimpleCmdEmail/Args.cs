aae393ab89881a1d4870a694c208e4a3211db6c2
Eli Hayon <Eli.Hayon@HealthTrustWs.com>
2018-10-23 14:43:27 -0400

    Adding an incredibly simplistic executable for sending email via .Net. It just seems like every other approach is too complicated for its own good.

---
+++ SimpleCmdEmail/Args.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimpleCmdEmail
+{
+    public class Args
+    {
+        public Args(string[] args)
+        {
+            SmtpServer = args[0];
+            Port = Convert.ToInt32(args[1]);
+            From = args[2];
+            To = args[3];
+            Subject = args[4];
+            Body = args[5];
+        }
+
+        public string SmtpServer { get; set; }
+
+        public int Port { get; set; }
+
+        public string From { get; set; }
+            
+        public string To { get; set; }
+            
+        public string Subject { get; set; }
+            
+        public string Body { get; set; }
+    }
+}
---
+++ SimpleCmdEmail/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace SimpleCmdEmail
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Args(args);
+
+            var dtmStart = DateTime.Now;
+            
+            //From, To, Host, Port, Subject, Body
+            Console.WriteLine($"Start @ {dtmStart}");
+	
+            SendEmail(a); //Works just fine
+	
+            var dtmEnd = DateTime.Now;
+
+            var ts = (dtmEnd - dtmStart);
+
+            Console.WriteLine($"End @ {dtmEnd} - {ts}");
+        }
+
+        public static void SendEmail(Args arg)
+        {
+            using (var client = new SmtpClient())
+            {
+                using (var mail = new MailMessage(arg.From, arg.To))
+                {
+                    client.Port = arg.Port;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Host = arg.SmtpServer;
+                    
+                    mail.Subject = arg.Subject;
+                    mail.Body = arg.Body;
+                    
+                    client.Send(mail);
+                }
+            }
+        }
+    }
+}
