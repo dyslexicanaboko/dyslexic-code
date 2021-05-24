1ae385e7b4a96e479768f555364a2714b405065c
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2021-05-24 00:45:22 -0400

    Migrated many things to new repositories. Outright deleting old things that I don't care about anymore. I might have to review this repository again at another time. More things might be able to be pulled out of this repository but I am not sure right now.

--- LinqPadScripts/Generate CSV data for Effort Framework/GetTables.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	   s.name AS SchemaName
-	  ,t.name AS TableName
-	  ,'[' + s.name + '].[' + t.name + ']' AS QualifiedSchemaAndTable
-FROM sys.tables t
-	INNER JOIN sys.schemas s
-		ON t.schema_id = s.schema_id
-WHERE t.name <> '__MigrationHistory'
